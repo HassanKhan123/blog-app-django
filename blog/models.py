5518115926408688ce9d8c7b3e946ef84429789e
HassanKhan123 <hk260280@gmail.com>
2025-06-10 19:23:06 +0200

    added: models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(max_length=200, unique=True,)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(
+        'Author', on_delete=models.SET_NULL, related_name='posts', null=True)
+    tags = models.ManyToManyField(
+        'Tag')
+
+    def __str__(self):
+        return self.title
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.caption
