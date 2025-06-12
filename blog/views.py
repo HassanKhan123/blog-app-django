d930e51239b91d5ab46d91e1b8ceb4d51adfdd88
HassanKhan123 <hk260280@gmail.com>
2025-06-12 14:33:19 +0200

    converted to class based views

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,34 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
 
-def starting_page(request):
-    all_posts = Post.objects.all().order_by('-date')[:3]
+class StartingPageView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    ordering = ['-date']
 
-    return render(request, 'blog/index.html', {
-        'posts': all_posts
-    })
-
-
-def posts(request):
-    all_posts = Post.objects.all().order_by('-date')
-    return render(request, 'blog/all-posts.html', {
-        'all_posts': all_posts
-    })
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
 
 
-def post_detail(request, slug):
-    post = get_object_or_404(Post, slug=slug)
-    return render(request, 'blog/post-detail.html', {'post': post, "post_tags": post.tags.all()})
+class AllPostsView(ListView):
+    model = Post
+    template_name = 'blog/all-posts.html'
+    context_object_name = 'all_posts'
+    ordering = ['-date']
+
+
+class SinglePostView(DetailView):
+    model = Post
+    template_name = 'blog/post-detail.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post_tags'] = self.object.tags.all()
+        return context
