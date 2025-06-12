0d22987534d3b04e442dc1aea7bf7aaa90dc9a04
HassanKhan123 <hk260280@gmail.com>
2025-06-12 16:19:48 +0200

    added: comment form

--- blog/models.py
+++ blog/models.py
@@ -31,3 +31,11 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.caption
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+    user_name = models.CharField(max_length=100)
+    user_email = models.EmailField()
+    text = models.TextField(max_length=400)
